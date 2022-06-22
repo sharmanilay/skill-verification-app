e14e57e3936347f5d65bd5c8f174467a36962605
Nilay Sharma <nilaysharma642@gmail.com>
2022-06-22 14:25:51 +0530

    add functions and modifier

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.1;
 
-contact Main {
+contract Main {
   company[] public companies;
   user[] public employees;
   certificate[] public certifications;
@@ -68,6 +68,15 @@ contact Main {
     uint256[] skill_endorsements;
   }
 
+  constructor() {
+    user storage dummy_user = employees.push();
+    dummy_user.name = "dummy";
+    dummy_user.wallet_address = msg.sender;
+    dummy_user.id = 0;
+    dummy_user.user_skills = new uint256[](0);
+    dummy_user.user_work_experience = new uint256[](0);
+  }
+
   function sign_up (
       stirng calldata email,
       string calldata name,
@@ -119,6 +128,116 @@ contact Main {
     address_to_id[msg.sender] = 0;
     address_to_id[new_address] = id;
   }
+
+  function add_experience (
+    uint256 user_id,
+    string calldata starting_date,
+    string calldata ending_date,
+    uint256 company_id
+  ) public verifiedUser (user_id) {
+    experience storage new_experience = experiences.push();
+    new_experience.company_id = company_id;
+    new_experience.currently_working = false;
+    new_experience.is_approved = false;
+    new_experience.starting_date = starting_date;
+    new_experience.role = role;
+    new_experience.ending_date = ending_date;
+    employees[user_id].user_work_experience.push(experiences.length - 1);
+    companies[company_id].requested_employees.push(experiences.length - 1);
+  }
+
+  function approve_experience(uint256 exp_id, uint256 company_id) public {
+	  require(
+	      (is_company[msg.sender] &&
+	          companies[address_to_id[msg.sender]].id ==
+	          experiences[exp_id].company_id) ||
+	          (employees[address_to_id[msg.sender]].is_manager &&
+	              employees[address_to_id[msg.sender]].company_id ==
+	              experiences[exp_id].company_id),
+	      "error: approver should be the company account or a manager of the required company"
+	  );
+    uint256 i;
+    experiences[exp_id].is_approved = true;
+    for (i = 0; i < companies[company_id].requested_employees.length; i++) {
+        if (companies[company_id].requested_employees[i] == exp_id) {
+            companies[company_id].requested_employees[i] = 0;
+            break;
+        }
+    }
+    for (i = 0; i < companies[company_id].current_employees.length; i++) {
+        if (companies[company_id].current_employees[i] == 0) {
+            companies[company_id].requested_employees[i] = exp_id;
+            break;
+        }
+    }
+    if (i == companies[company_id].current_employees.length)
+        companies[company_id].current_employees.push(exp_id);
+  } 
+
+  function approve_manager(uint256 employee_id) public {
+		require(is_company[msg.sender], "error: sender not a company account");
+    require(
+        employees[employee_id].company_id == address_to_id[msg.sender],
+        "error: user not of the same company"
+    );
+    require(
+        !(employees[employee_id].is_manager),
+        "error: user is already a manager"
+    );
+    employees[employee_id].is_manager = true;
+  }
+
+  function add_skill(uint256 userid, string calldata skill_name)
+    public
+    verifiedUser(userid) { // the modifier that we created above
+    skill storage new_skill = skills.push();
+    employees[userid].user_skills.push(skills.length - 1);
+    new_skill.name = skill_name;
+    new_skill.verified = false;
+    new_skill.skill_certifications = new uint256[](0);
+    new_skill.skill_endorsements = new uint256[](0);
+  }
+
+  function add_certification(
+      uint256 user_id,
+      string calldata url,
+      string calldata issue_date,
+      string calldata valid_till,
+      string calldata name,
+      string calldata issuer,
+      uint256 linked_skill_id
+  ) public verifiedUser(user_id) {
+      certificate storage new_certificate = certifications.push();
+      new_certificate.url = url;
+      new_certificate.issue_date = issue_date;
+      new_certificate.valid_till = valid_till;
+      new_certificate.name = name;
+      new_certificate.id = certifications.length - 1;
+      new_certificate.issuer = issuer;
+      skills[linked_skill_id].skill_certifications.push(new_certificate.id);
+  }
+
+  function endorse_skill(
+      uint256 user_id,
+      uint256 skill_id,
+      string calldata endorsing_date,
+      string calldata comment
+  ) public {
+      endorsment storage new_endorsemnt = endorsments.push();
+      new_endorsemnt.endorser_id = address_to_id[msg.sender];
+      new_endorsemnt.comment = comment;
+      new_endorsemnt.date = endorsing_date;
+      skills[skill_id].skill_endorsements.push(endorsments.length - 1);
+      if (employees[address_to_id[msg.sender]].is_manager) {
+          if (
+              employees[address_to_id[msg.sender]].company_id ==
+              employees[user_id].company_id
+          ) {
+              skills[skill_id].verified = true;
+          }
+      }
+  }
+
 
   modifier verifiedUser (uint256 user_id) {
     require(user_id == address_to_id[msg.sender]);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
+import React, { useState, useEffect } from 'react'
+import Web3 from 'web3'
+import SmartContract from '../abis/Main.json'
 
+const App = () => {
+  const [state, setState] = useState({
+    web3: null,
+    contract: null,
+    email: '',
+    account: '',
+    accountId: '',
+    signedIn: false,
+    loaded: false,
+  })
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  const initWeb3 = async () => {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      try {
+        const web3 = new Web3(window.ethereum)
+        const account = (await web3.eth.getAccounts())[0]
+        const netId = await web3.eth.net.getId()
+        const address = SmartContract.networks[netId].address
+        const contract = new web3.eth.Contract(SmartContract.abi, address)
+        const accountId = await contract.methods.address_to_id(account).call()
+        setState({
+          ...state,
+          web3,
+          account,
+          contract,
+          accountId,
+          loaded: true,
+        })
+        console.log('setup complete')
+      } catch (e) {
+        alert(e)
+      }
+    } else {
+      alert('web3 not detected')
+    }
+  }
+
+  const login = async () => {
+    try {
+      const accountType = await state.contract.methods.login(state.email).call({
+        from: state.account,
+      })
+      console.log('account type:', accountType)
+      setState({ ...state, signedIn: true })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const signUp = async () => {
+    try {
+      await state.contract.methods
+        .sign_up(state.email, 'name', 'user')
+        .send({ from: state.account })
+      alert('signed up')
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const requestCompany = async (startDate, endDate, role, companyId) => {
+    try {
+      await state.contract.methods.add_experience(
+        state.accountId,
+        startDate,
+        endDate,
+        role,
+        companyId,
+      )
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const approveEmployee = async (experienceId, companyId) => {
+    try {
+      await state.contract.methods.approve_experience(experienceId, companyId)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const updateWallet = async (newAddress) => {
+    try {
+      await state.contract.methods.update_wallet_address(
+        state.email,
+        newAddress,
+      )
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return <div>App</div>
 }
 
-export default App;
+export default App
